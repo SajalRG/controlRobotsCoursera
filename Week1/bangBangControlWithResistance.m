efaf95a23b8808347a672e47d1a7824eb92439af
VitoWU <vitowu@bupt.edu.cn>
2016-05-10 23:01:27 +0800

    Update bangBangControlWithResistance.m
    And change the "totalSamples" at "ControlMain.m" to 10000. Using RK-4 (Fourth order Runge-Kutta) to get the slope may also help.

--- Week1/bangBangControlWithResistance.m
+++ Week1/bangBangControlWithResistance.m
@@ -16,7 +16,7 @@ function [controlInput, controlOutput] = bangBangControlWithResistance(totalSamp
 	magicConstant = c/m; #from the lecture
 	
 	#this is random constant, feel free to play with it
-	dT = 0.05;
+	dT = 0.001;
 	
 	#k is some resistance constant
 	k = 0.3;
@@ -45,8 +45,45 @@ function [controlInput, controlOutput] = bangBangControlWithResistance(totalSamp
 		endif
 		
 		controlInput(i) = u;
-		controlOutput(i+1) = controlOutput(i) + dT * (magicConstant * u
-							- k * controlOutput(i));
+	        #RK-4
+	        
+	        #calculate A
+	        A = dT * (magicConstant * u - k * controlOutput(i));
+        	#calculate B
+        	controlOutput_temp(i+1)=controlOutput(i)+A;
+	        if(controlOutput_temp(i+1)<r)
+	            u=100;
+	        elseif(controlOutput_temp(i+1)>r)
+	            u=-100;
+	        else
+	            u=0;
+	        endif
+	        B = magicConstant * u - k * controlOutput_temp(i+1);
+	        #calculate C
+	        controlOutput_temp(i+2)=controlOutput_temp(i+1)+0.5*(A+B)*dT;
+	        if(controlOutput_temp(i+2)<r)
+	            u=100;
+	        elseif(controlOutput_temp(i+2)>r)
+	            u=-100;
+	        else
+	            u=0;
+	        endif
+	        C=magicConstant * u - k * controlOutput_temp(i+2);
+	        #calculate D
+	        controlOutput_temp(i+3)=controlOutput_temp(i+2)+(1/3)*(A+B+C)*dT;
+	        if(controlOutput_temp(i+3)<r)
+	            u=100;
+	        elseif(controlOutput_temp(i+3)>r)
+	            u=-100;
+	        else
+	            u=0;
+	        endif
+	        D=magicConstant * u - k * controlOutput_temp(i+3);
+	        
+	        controlOutput(i+1) = controlOutput(i) + dT * (A+2*B+2*C+D)*(1/6);
+
+		#controlOutput(i+1) = controlOutput(i) + dT * (magicConstant * u
+							#- k * controlOutput(i));
 		controlInput(i+1) = 0;
     endfor
 endfunction
